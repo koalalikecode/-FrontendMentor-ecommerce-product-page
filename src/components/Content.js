2786db51dda997c9309888e9c555143f5facd200
Duy Nguyễn <duykhanhchi1993@gmail.com>
2021-12-23 18:59:40 +0700

    fix UI

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,8 +8,10 @@ function Content() {
                 <p className="company">SNEAKER COMPANY</p>
                 <h1 className="product-name">Fall Limited Edition Sneakers</h1>
                 <p className="description">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
-                <h2 className="price">$125.00</h2>
-                <p className="past-price">$250.00</p>
+                <div className="price-container">
+                    <h2 className="price">$125.00</h2>
+                    <p className="past-price">$250.00</p>
+                </div>
             </div>
             <Buying />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
+import { useState } from 'react'
+
 import logo from '../images/logo.svg'
 import ava from '../images/image-avatar.png'
+import menu_icon from '../images/icon-menu.svg'
+import close from '../images/icon-close.svg'
 import '../styles/Navbar.scss'
 
 import Cart from './Cart.js'
 
 function Navbar() {
+    const [menu, setMenu] = useState(false)
     return (
         <div className="navbar">
+            <div class='menu-btn'>
+                <img src={menu_icon} alt='menu' onClick={() => setMenu(true)}/>
+            </div>
             <div className = "logo" onClick={() => console.log(1)}>
                 <img src={logo} alt = 'Logo'/>
 
             </div>
-            <ul className='menu'>
-                <li>Collections</li>
-                <li>Men</li>
-                <li>Women</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <div className={menu ? 'menu-container menu-active' : 'menu-container'}>
+                <ul className='menu'>
+                    <img src={close} alt = 'Close' onClick={() => setMenu(false)}/>
+                    <li>Collections</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
 
             <Cart />
             
